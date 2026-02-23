8c06256a0f7b3ac37363b40f699eb4efb8fc6652
alex12345-123 <alex.tashiguano@educa.madrid.es>
2026-02-23 11:12:37 +0100

    Cambios en Hercules

--- hercules/player.gd
+++ hercules/player.gd
@@ -1,76 +1,30 @@
 extends CharacterBody2D
-@export var jump_force = -700
-@export var air_acceleration = 2000
-@export var air_friction = 700
-@export var friction = 1500
-@export var gravity_scale = 2
-
 
 @export var speed = 500
 @export var acceleration = 600
+@export var friction = 1500
 
-func apply_gravity(delta):
-	if not is_on_floor():
-		velocity+=get_gravity() * delta * gravity_scale
+@export var air_acceleration = 2000
+@export var air_friction = 700
 
+@export var jump_force = -700
+@export var gravity_scale = 2
 
-func handle_jump():
-	if is_on_floor():
-		if Input.is_action_pressed("saltar"):
-			velocity.y = jump_force
-			
-func handle_acceleration(input_axis, delta):
-	if not is_on_floor(): return
-	if input_axis != 0:
-		velocity.x = move_toward(velocity.x, speed*input_axis, acceleration*delta)
-
-	
-func handle_air_acceleration(input_axis, delta):
-	if is_on_floor(): return
-	if input_axis != 0:
-		velocity.x = move_toward(velocity.x, speed*input_axis, air_acceleration *delta)
-		
-
-func apply_friction(input_axis, delta):
-	if input_axis==0 and is_on_floor():
-		velocity.x = move_toward(velocity.x, 0, friction*delta)
-
-
-@onready var ani_player = $AnimatedSprite2D
-
-func update_animation(input_axis):
-	if input_axis !=0:
-		# velocidad de la animación será dependiente de la velocidad
-		ani_player.speed_scale = velocity.length()/100
-		ani_player.flip_h = (input_axis<0)
-		ani_player.play("carrera")
-	elif not is_on_floor():
-		ani_player.play("saltar")
-	else:
-		ani_player.speed_scale=1
-		ani_player.play("reposo")
-
+@export var damage = 1
+@export var max_health := 100
 
 var attacking = false
-@export var damage = 1
+var health := max_health
 
+@onready var ani_player = $AnimatedSprite2D
 @onready var attack_area = $ataque
+@onready var health_bar = $CanvasLayer/ProgressBar
+
 
 func _ready():
 	attack_area.monitoring = false
 	update_health_bar()
 
-func attack():
-	attacking = true
-	velocity.x = 0
-	ani_player.speed_scale = 1
-	ani_player.play("puño") 
-	attack_area.monitoring = true
-
-	await ani_player.animation_finished
-
-	attack_area.monitoring = false
-	attacking = false
 
 func _physics_process(delta: float) -> void:
 	var input_axis = Input.get_axis("mover_izquierda","mover_derecha")
@@ -89,13 +43,65 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity += get_gravity() * delta * gravity_scale
+
+
+func handle_jump():
+	if is_on_floor() and Input.is_action_pressed("saltar"):
+		velocity.y = jump_force
+
+
+func handle_acceleration(input_axis, delta):
+	if not is_on_floor():
+		return
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, speed * input_axis, acceleration * delta)
+
+
+func handle_air_acceleration(input_axis, delta):
+	if is_on_floor():
+		return
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, speed * input_axis, air_acceleration * delta)
+
+
+func apply_friction(input_axis, delta):
+	if input_axis == 0 and is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, friction * delta)
+
+
+func update_animation(input_axis):
+	if input_axis != 0:
+		ani_player.speed_scale = velocity.length() / 100
+		ani_player.flip_h = (input_axis < 0)
+		ani_player.play("carrera")
+	elif not is_on_floor():
+		ani_player.play("saltar")
+	else:
+		ani_player.speed_scale = 1
+		ani_player.play("reposo")
+
+
+func attack():
+	attacking = true
+	velocity.x = 0
+
+	ani_player.speed_scale = 1
+	ani_player.play("puño")
+	attack_area.monitoring = true
+
+	await ani_player.animation_finished
+
+	attack_area.monitoring = false
+	attacking = false
+
 
 func _on_ataque_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemies"):
 		body.take_damage(damage)
 
-@export var max_health := 100
-var health := max_health
 
 func take_damage(amount: int):
 	health -= amount
@@ -105,10 +111,10 @@ func take_damage(amount: int):
 	if health <= 0:
 		die()
 
+
 func die():
-	queue_free() 
-	
-@onready var health_bar = $CanvasLayer/ProgressBar
+	queue_free()
+
 
 func update_health_bar():
 	if health_bar:
