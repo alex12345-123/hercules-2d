0f0219f1b8ae007c83e0d2e8b3e1ae029464d6cc
sancha-aesthetic <joseantonio.sanchachausal@educa.madrid.org>
2026-02-24 01:36:13 +0100

    cambio de mundo y mejoras en proyecto

---
+++ enviroment/change_scene.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+
+@export var next_scene_path: String
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		change_scene()
+
+func change_scene():
+	get_tree().change_scene_to_file(next_scene_path)
