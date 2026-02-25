c875d76621921b7af799375dc707f62de4a68e9e
Simon212121 <simonsergio19@gmail.com>
2026-02-25 02:16:49 +0100

    vida_enemigos

--- minotauro.gd
+++ minotauro.gd
@@ -3,10 +3,17 @@ extends CharacterBody2D
 @onready var gravity = ProjectSettings.get("physics/2d/default_gravity")
 @onready var ani = $ani_minotauro
 @onready var area_vision = $Area2D
+@export var max_health := 100
+var health := 100
 
+@onready var barra_vida = $barraVida
+
+var img_100 = preload("res://enemigos/barraVidaEne/vida100.png")
+var img_50 = preload("res://enemigos/barraVidaEne/vida50.png")
+var img_0  = preload("res://enemigos/barraVidaEne/vida0.png")
 @export var speed = 100
 @export var damage_al_jugador = 10
-@export var health = 50 
+
 
 var sentido = 1
 var atacando = false
@@ -15,6 +22,8 @@ var atacando = false
 func _ready():
 	area_vision.monitoring = true
 	add_to_group("enemies")
+	health = max_health
+	actualizar_interfaz_vida()
 
 
 func _physics_process(delta):
@@ -66,16 +75,33 @@ func hacer_ataque(objetivo):
 
 
 func take_damage(amount):
-	health -= amount
-	print("Minotauro herido! Vida restante: ", health)
+	health -= 50  # Cada golpe baja exactamente 50%
+	health = clamp(health, 0, max_health)
+
+	print("Vida Minotauro: ", health)
 
 	ani.modulate = Color.RED
 	await get_tree().create_timer(0.1).timeout
 	ani.modulate = Color.WHITE
 
+	actualizar_interfaz_vida()
+
 	if health <= 0:
 		die()
+func actualizar_interfaz_vida():
+	if not barra_vida:
+		return
 
+	var porcentaje = (float(health) / max_health) * 100.0
+
+	if porcentaje >= 100:
+		barra_vida.texture = img_100
+	elif porcentaje >= 50:
+		barra_vida.texture = img_50
+	elif porcentaje > 0:
+		barra_vida.texture = img_50
+	else:
+		barra_vida.texture = img_0
 
 func die():
 	queue_free()
